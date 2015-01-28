f4bd965158454e77c3bdc17e552ebe47cc87fc3d
Dante-Vonarmia <dantestylish@hotmail.com>
2015-01-28 10:58:02 +0800

    dxGame
    GameCore

---
+++ Project_DXGame_Demo v1.0/GameCore/Core_Global.h
@@ -0,0 +1,34 @@
+/**************************************************
+WinMain.cpp
+GameCore Component
+
+Programming Role-Playing Games with DirectX
+by Jim Adams (01 Jan 2002)
+**************************************************/
+
+#ifndef _CORE_GLOBAL_H_
+#define _CORE_GLOBAL_H_
+
+// Windows includes
+#include <windows.h>
+
+// Standard ANSI-C includes
+#include <stdio.h>
+
+// DirectX includes
+#include "d3d8.h"
+#include "d3dx8.h"
+#include "dmusici.h"
+#include "dsound.h"
+#include "dplay8.h"
+#include "dpaddr.h"
+#include "dinput.h"
+
+// Core includes
+#include "Core_System.h"
+#include "Core_Graphics.h"
+#include "Core_Input.h"
+#include "Core_Sound.h"
+#include "Core_Network.h"
+
+#endif
